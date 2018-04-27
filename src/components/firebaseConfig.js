68ba9010c8d480093d39f22343e3fa87919bc772
Sourav Dutta <souravbpd@gmail.com>
2018-04-27 21:48:29 +0530

    Employee Manager CRUD app using vue.js and firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCCD088nOY3kMaHY0xI9vScq3duBE_gM9k",
+  authDomain: "vue-firebase-crud-demo.firebaseapp.com",
+  databaseURL: "https://vue-firebase-crud-demo.firebaseio.com",
+  projectId: "vue-firebase-crud-demo",
+  storageBucket: "vue-firebase-crud-demo.appspot.com",
+  messagingSenderId: "325795354501"
+};
